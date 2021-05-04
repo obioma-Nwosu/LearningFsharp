7de5f8d0f8c994ce5b4453f7c5482575a38edfab
obi <obiomanwosu.nwosu@siemens-healthineers.com>
2021-05-05 00:33:23 +0200

    new file changes

---
+++ forloop.fs
@@ -0,0 +1,24 @@
+namespace forloop
+
+//for i in 0..argv.Length-1 do
+//let person = argv.[i]
+//printfn "Hello %s, Nice to meet you" person
+// printfn "Thanks and Goodbye"
+//0
+
+//For loops with iterators a better approach
+
+//for person in argv do
+//printfn "Hello %s, Nice to meet you" person
+// printfn "Thanks and Goodbye"
+//0
+
+//iterating with Array.iter
+
+//let sayHello person =
+//printfn "Hello %s from my f# program!" person
+
+//let main argv =
+//Array.iter SayHello argv
+//printfn "Nice to meet you."
+//0
---
+++ fowardPiping.fs
@@ -0,0 +1,25 @@
+namespace fowardPiping
+
+//let sayHello person =
+//printfn "Hello %s" person
+
+//let isvalid person =
+//not(String.IsNullOrWhiteSpace Person)
+
+//let isAllowed person =
+//person <> eve             //<> means not equal
+
+
+
+//let main argv =
+//let validNames = argv |> Array.filter isValid
+//validNames |> Array.iter sayHello
+
+//OR IN A SIMPLER WAY
+
+//argv
+//|> Array.filter isValid
+//|> Array.filter isAllowed
+//|> Array.iter sayHello
+//printfn "Nice to meet you"
+//0
